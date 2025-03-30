5ea0fa8cc488efc7be627a608e4beef4d127bcbf
DemonExposer <kbdmns@gmail.com>
2025-03-30 20:29:24 +0200

    QR code scanning

--- Nockx.Android/MainActivity.cs
+++ Nockx.Android/MainActivity.cs
@@ -1,11 +1,24 @@
 using Android.Content.PM;
 using Avalonia.Android;
 using Avalonia.Controls;
+using Xamarin.Essentials;
+using ZXing.Mobile;
+using Result = ZXing.Result;
 
 namespace Nockx.Android;
 
 [Activity(Label = "@string/app_name", Theme = "@style/MyTheme.NoActionBar", MainLauncher = true, 
 	ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-public class MainActivity : AvaloniaMainActivity<App> {
-	
+public class MainActivity : Activity {
+	protected override async void OnCreate(Bundle? bundle) {
+		base.OnCreate(bundle);
+		Platform.Init(this, bundle);
+		
+		MobileBarcodeScanner scanner = new ();
+		Result result = await scanner.Scan();
+
+		if (result != null) {
+			Console.WriteLine("code: {0}", result.Text);
+		}
+	}
 }
