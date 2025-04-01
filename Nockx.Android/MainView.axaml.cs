aa3ae7d11efefed322a164a0e16374a4eb907744
DemonExposer <56930597+DemonExposer@users.noreply.github.com>
2025-04-01 23:58:54 +0200

    Updated LessAnnoyingHttp

--- Nockx.Android/MainView.axaml.cs
+++ Nockx.Android/MainView.axaml.cs
@@ -9,9 +9,9 @@ using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Security;
 using System.Net;
-using System.Text;
 using System.Web;
 using Org.BouncyCastle.Asn1.Pkcs;
+using LessAnnoyingHttp;
 
 namespace Nockx.Android;
 
@@ -67,84 +67,9 @@ public partial class MainView : Panel {
 	public bool RequestPrivateKey(RsaKeyParameters foreignPublicKey) {
 		long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 		string getVariables = $"requestingUser={HttpUtility.UrlEncode(_personalPublicKey.ToBase64String())}&requestedUser={HttpUtility.UrlEncode(foreignPublicKey.ToBase64String())}&timestamp={timestamp}";
-		Response response = Get($"https://...:5000/privateKeyRequest?" + getVariables, [new Header { Name = "Signature", Value = Cryptography.Sign(timestamp.ToString(), _privateKey) }]);
+		Response response = Http.Get($"https://...:5000/privateKeyRequest?" + getVariables, [new Header { Name = "Signature", Value = Cryptography.Sign(timestamp.ToString(), _privateKey) }]);
 		Console.WriteLine("{0} {1}", response.StatusCode, response.Exception);
 
 		return response.StatusCode == HttpStatusCode.OK;
 	}
-
-	private class Response {
-		/// <summary>
-		/// The response of the HTTP request (is empty when the request failed)
-		/// </summary>
-		public string Body { get; internal init; } = "";
-
-		/// <summary>
-		/// Specifies whether the request was successful
-		/// </summary>
-		public bool IsSuccessful { get; internal init; }
-
-		/// <summary>
-		/// The HTTP status code returned by the server
-		/// </summary>
-		public HttpStatusCode StatusCode { get; internal init; }
-
-		/// <summary>
-		/// Any exception that was thrown during the sending of the request (is null when no exception was thrown)
-		/// </summary>
-		public Exception? Exception { get; internal init; }
-
-		internal Response() { }
-	}
-
-	private static Response Get(string endpoint, Header[]? headers = null) {
-		using HttpClient client = new();
-		client.Timeout = TimeSpan.FromSeconds(10000);
-		HttpRequestMessage request = new() {
-			RequestUri = new Uri(endpoint),
-			Method = HttpMethod.Get
-		};
-
-		if (headers != null)
-			foreach (Header header in headers)
-				request.Headers.Add(header.Name, header.Value);
-
-		HttpResponseMessage response;
-		try {
-			response = client.SendAsync(request).Result;
-		} catch (TaskCanceledException) {
-			throw new TimeoutException($"Timeout waiting for response for request to {endpoint}");
-		} catch (Exception e) {
-			return new Response { IsSuccessful = false, Body = "", Exception = e };
-		}
-
-		return new Response { IsSuccessful = response.IsSuccessStatusCode, Body = response.Content.ReadAsStringAsync().Result, StatusCode = response.StatusCode };
-	}
-
-	private static Response BodyRequest(string endpoint, HttpMethod method, string body, Header[]? headers, string contentType) {
-		using HttpClient client = new();
-		client.Timeout = TimeSpan.FromSeconds(10000);
-		HttpRequestMessage request = new() {
-			RequestUri = new Uri(endpoint),
-			Method = method,
-			Content = new StringContent(body, Encoding.UTF8, contentType)
-		};
-
-		if (headers != null)
-			foreach (Header header in headers)
-				request.Headers.Add(header.Name, header.Value);
-
-		HttpResponseMessage response;
-		try {
-			response = client.SendAsync(request).Result;
-		} catch (TaskCanceledException) {
-			throw new TimeoutException($"Timeout waiting for response for request to {endpoint}");
-		} catch (Exception e) {
-			return new Response { IsSuccessful = false, Body = "", Exception = e };
-		}
-
-		return new Response { IsSuccessful = response.IsSuccessStatusCode, Body = response.Content.ReadAsStringAsync().Result, StatusCode = response.StatusCode };
-	}
-
-	private static Response Post(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => BodyRequest(endpoint, HttpMethod.Post, body, headers, contentType);
 }
